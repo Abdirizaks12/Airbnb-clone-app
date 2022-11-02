e2f7fc5258627a15e4118f8ee55e4759483343da
Abdirizak <abdirizaksigat99@gmail.com>
2022-11-02 19:32:46 +0300

    update img css

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import starImg from "../images/star.png"
 export default function Card(props) {
     let badgeText
     if (props.openSpots === 0) {
@@ -15,11 +15,11 @@ export default function Card(props) {
                 <div className="card--badge">{badgeText}</div>
             }
             <img 
-                src={`../images/${props.coverImg}`} 
+                src={props.coverImg} 
                 className="card--image" 
             />
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
+                <img src={starImg}className="card--star" />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
